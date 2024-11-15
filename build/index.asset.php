20d56c7a538221a6a144e2697de8be526c3930d4
Herb Miller <herb@bobbingwide.com>
2024-11-15 16:10:00 +0000

    Update build #6

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3e6c1c9cb04614ea4aabeecde19fc808');
---
+++ build/sb-field-block.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f3ff68d6bb5469037a9b');
