3a0572ef829e40df4b0c0b74ba54a1fc4246b8ac
Herb Miller <herb@bobbingwide.com>
2024-11-18 12:28:34 +0000

    Obtain post type label from the oiksb/fields store #9

--- src/sb-field-block/post_type.js
+++ src/sb-field-block/post_type.js
@@ -3,7 +3,7 @@
  *
  * Similar to field.js with code copied from oik-content/post_type.js ( oik-blocks )
  *
- * @copyright (C) Copyright Bobbing Wide 2021
+ * @copyright (C) Copyright Bobbing Wide 2021, 2024
  * @author Herb Miller @bobbingwide
  */
 
@@ -18,14 +18,22 @@ import { SelectControl } from '@wordpress/components';
 	 * @returns {{label: *, value: *}}
 	 */
 function postTypeOption( postType ) {
-		return( { value: postType.slug, label: postType.name });
+		return( { value: postType.postType, label: postType.label });
 }
 
 
 export const PostTypeSelect = ( props ) => {
+	/*
 	const postTypes = useSelect((select) => {
 		return select('core').getPostTypes({per_page: -1});
 	}, []);
+
+	 */
+
+	const postTypes = useSelect( ( select ) => {
+		return select( 'oiksb/fields' ).getFields();
+	}, []);
+
 	console.log("PostTypeSelect");
 	console.log(postTypes);
 	console.log( props);
